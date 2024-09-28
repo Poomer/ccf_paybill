66d133fb02beb0c5f2328ad5a9bb21929ef9998a
Poom Poomer <v.nichayapun@gmail.com>
2024-09-28 14:17:15 +0700

    Add function to calculate mean

--- paybill.py
+++ paybill.py
@@ -1,5 +1,11 @@
-import pandas as pd 
-
 income = [500, 600, 600]
 
-y = mean(income)
+for money in income:
+    print(money)
+
+# Add a function to calculate the mean of salary
+def calculate_mean(income_list):
+    return sum(income_list) / len(income_list)
+
+
+print(calculate_mean(income))
